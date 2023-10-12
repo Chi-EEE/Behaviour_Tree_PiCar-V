641740ea231d963e393ad0fb269c1c0018fdb5c6
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-10-12 16:39:21 +0100

    Add TODO and imath package

--- TODO.md
+++ TODO.md
@@ -1,2 +1,7 @@
 # TODO List:
 
+Pass Lidar data from raspberry pi to backend
+
+Pass commands from backend to raspberry pi
+
+Display Lidar data in Svelte
--- raspberry-pi/xmake.lua
+++ raspberry-pi/xmake.lua
@@ -1,7 +1,11 @@
 add_rules("mode.debug", "mode.release")
+
+add_requires("imath")
 add_requires("sockpp")
 
 target("raspberry-pi")
     set_kind("binary")
     add_files("src/*.cpp")
+
+    add_packages("imath")
     add_packages("sockpp")
