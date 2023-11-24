83070b4aec1ef56517ad5bebe8a61f547182f202
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-11-24 09:34:35 +0000

    Remove oatpp

--- backend/xmake.lua
+++ backend/xmake.lua
@@ -6,40 +6,52 @@ add_repositories("repository repository")
 set_languages("cxx17")
 
 -- Logging / Data handling
-add_requires("boost", { configs = {chrono = true} })
-add_requires("fmt", "spdlog")
-add_requires("nlohmann_json")
-add_requires("vcpkg::cartographer", {alias = "cartographer"})
+-- add_requires("boost", { configs = {chrono = true} })
+-- add_requires("fmt", "spdlog")
+-- add_requires("nlohmann_json")
 
 -- C++ Backend API for Svelte App
-add_requires("oatpp", "oatpp-websocket")
+add_requires("drogon")
 
 -- For Functional Programming?
-add_requires("tl_expected")
+-- add_requires("tl_expected")
 
 target("backend")
     set_kind("binary")
 
-    add_packages("boost")
-    add_packages("fmt", "spdlog")
-    add_packages("nlohmann_json")
-    add_packages("oatpp", "oatpp-websocket")
-    add_packages("tl_expected")
-
-    add_packages("cartographer")
+    -- add_packages("boost")
+    -- add_packages("fmt", "spdlog")
+    -- add_packages("nlohmann_json")
+    add_packages("drogon")
+    -- add_packages("tl_expected")
 
     before_build_files(function(target)
+        import("core.project.config")
+        import("core.base.json")
+
+        config.load()
+
+        local exe_dir = path.join(os.projectdir(), config.get("buildir"), config.get("plat"), config.get("arch"), config.get("mode"))
+
+        local static = path.join(exe_dir, "public")
+
+        local config_table = json.loadfile(path.join(os.scriptdir(), "settings", "config.json"))
+        config_table["app"]["document_root"] = static
+        
+        local config_json = json.encode(config_table)
+
+        io.writefile(path.join(exe_dir, "settings", "config.json"), config_json)
+
         local frontend_dir = path.join(os.scriptdir(), "..", "frontend")
         os.execv("pnpm", {"--prefix", frontend_dir, "i"})
         os.execv("pnpm", {"--prefix", frontend_dir, "run", "build"})
+
         print("Copying svelte files...")
-        os.cp("$(scriptdir)/../frontend/public", "$(buildir)/$(plat)/$(arch)/$(mode)/frontend/public")
+        os.cp(path.join(os.scriptdir(), "..", "frontend", "public"), static)
         print("Completed copying svelte files...")
     end)
 
     add_files("src/**.cpp")
-    add_headerfiles("src/**.hpp")
-    add_files("src/controller/**.cc")
 
     add_includedirs("include")
 
@@ -47,24 +59,20 @@ target("backend")
         add_defines("_WIN32")
     end
 
-     set_configdir("$(buildir)/$(plat)/$(arch)/$(mode)/config")
-     add_configfiles("config/*.json", {onlycopy = true})
-
-    
-for _, file in ipairs(os.files("tests/test_*.cpp")) do
-    local name = path.basename(file)
-    target(name)
-        set_kind("binary")
-        set_default(false)
-        add_packages("boost")
-        add_packages("fmt", "spdlog")
-        add_packages("nlohmann_json")
-        add_packages("oatpp", "oatpp-websocket")
-        add_packages("tl_expected")
-        add_files("tests/" .. name .. ".cpp")
-        add_files("src/controller/**.cc")
-        -- add_tests("default")
-        -- add_tests("args", {runargs = {"foo", "bar"}})
-        -- add_tests("pass_output", {trim_output = true, runargs = "foo", pass_outputs = "hello foo"})
-        -- add_tests("fail_output", {fail_outputs = {"hello2 .*", "hello xmake"}})
-end
+-- for _, file in ipairs(os.files("tests/test_*.cpp")) do
+--     local name = path.basename(file)
+--     target(name)
+--         set_kind("binary")
+--         set_default(false)
+--         add_packages("boost")
+--         add_packages("fmt", "spdlog")
+--         add_packages("nlohmann_json")
+--         add_packages("oatpp", "oatpp-websocket")
+--         add_packages("tl_expected")
+--         add_files("tests/" .. name .. ".cpp")
+--         add_files("src/controller/**.cc")
+--         -- add_tests("default")
+--         -- add_tests("args", {runargs = {"foo", "bar"}})
+--         -- add_tests("pass_output", {trim_output = true, runargs = "foo", pass_outputs = "hello foo"})
+--         -- add_tests("fail_output", {fail_outputs = {"hello2 .*", "hello xmake"}})
+-- end
