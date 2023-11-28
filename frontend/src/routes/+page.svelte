5b0bd557cb37c56519d396b6010e121dfbbb7a5b
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-11-28 20:51:52 +0000

    Attempt websocket

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+    var webSocket = new WebSocket("./chat");
+    webSocket.addEventListener("message", (event) => {
+        console.log(event);
+    })
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
