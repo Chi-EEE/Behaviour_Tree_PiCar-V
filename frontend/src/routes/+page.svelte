e2fff742ce7fb19acf6dca740b21cdfb65183a34
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-11-29 00:43:10 +0000

    Can now use websocket

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
-    var webSocket = new WebSocket("./chat");
-    webSocket.addEventListener("message", (event) => {
-        console.log(event);
-    })
+    import { websocket_url } from './store.js';
+   
+    websocket_url.subscribe((value) => {
+        console.log(value);
+        try {
+            let webSocket = new WebSocket(value);
+            webSocket.addEventListener("open", (event) => {
+                console.log("Open");
+            });
+            webSocket.addEventListener("message", (event) => {
+                console.log(event);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    });
+    function set_websocket_url(event: Event & { target: HTMLInputElement }) {
+        websocket_url.set(event.target.value);
+    }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<input on:input={set_websocket_url} />
