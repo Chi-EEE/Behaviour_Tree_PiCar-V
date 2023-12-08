613973d7d35699fad1096cb48258136e9b82684c
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-12-08 23:12:26 +0000

    Update test.cpp

--- libraries/RPLidar/test/test.cpp
+++ libraries/RPLidar/test/test.cpp
@@ -7,13 +7,13 @@ int main()
 {
     spdlog::set_level(spdlog::level::debug);
 
-    auto lidar_result = RPLidar::create("/dev/ttyUSB0");
+    // auto lidar_result = RPLidar::create("/dev/ttyUSB0");
     auto lidar_result = RPLidar::create("COM3");
     if (!lidar_result.has_value())
     {
         return 0;
     }
-    auto lidar = std::move(lidar_result.value());
+    auto& lidar = std::move(lidar_result.value());
 
     auto info_result = lidar->get_info();
     if (!info_result.has_value())
@@ -21,7 +21,7 @@ int main()
         std::cout << info_result.error();
         return 0;
     }
-    auto info = info_result.value();
+    auto& info = info_result.value();
     std::cout << fmt::format("model: {}, firmware: ({}, {}), hardware: {}, serialnumber: {}\n", info.model, info.firmware.first, info.firmware.second, info.hardware, info.serialNumber);
 
     auto health_result = lidar->get_health();
@@ -30,23 +30,23 @@ int main()
         std::cout << health_result.error();
         return 0;
     }
-    auto health = health_result.value();
+    auto& health = health_result.value();
     std::cout << fmt::format("({}, {})\n", health.status, health.errorCode);
 
-    std::function<std::vector<Measure>()> scanGenerator = lidar->iter_scans();
-    for (int i = 0; i < 10; i++)
-    {
-        std::vector<Measure> scan = scanGenerator();
-        std::cout << "Got " << scan.size() << " Measures!\n";
-        for (const Measure &measure : scan)
-        {
-            // Access individual measurements in the scan
-            bool newScan = measure.newScan;
-            int quality = measure.quality;
-            float angle = measure.angle;
-            float distance = measure.distance;
-        }
-    }
+    // std::function<std::vector<Measure>()> scanGenerator = lidar->iter_scans();
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     std::vector<Measure> scan = scanGenerator();
+    //     std::cout << "Got " << scan.size() << " Measures!\n";
+    //     for (const Measure &measure : scan)
+    //     {
+    //         // Access individual measurements in the scan
+    //         bool newScan = measure.newScan;
+    //         int quality = measure.quality;
+    //         float angle = measure.angle;
+    //         float distance = measure.distance;
+    //     }
+    // }
 
     lidar->stop();
     lidar->stop_motor();
