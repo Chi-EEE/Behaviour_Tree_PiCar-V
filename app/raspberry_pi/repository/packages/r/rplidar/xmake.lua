7f0ae100c331f70cbe37cfdd48a27cf0aab9c40c
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-12-14 19:11:20 +0000

    Fix install RPLidar for windows

--- app/raspberry_pi/repository/packages/r/rplidar/xmake.lua
+++ app/raspberry_pi/repository/packages/r/rplidar/xmake.lua
@@ -1,9 +1,9 @@
 package("rplidar")
     add_deps("serial", "spdlog", "tl_expected")
-    set_sourcedir(path.join(os.scriptdir(), "rplidar"))
     set_policy("package.install_always", true)
     on_install(function (package)
         local configs = {}
+        os.cd(path.join(os.scriptdir(), "rplidar"))
         import("package.tools.xmake").install(package, configs)
     end)
 package_end()
