c2b9749b24f7380aa0dab17481a5d78e5a9880db
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-01-03 03:43:50 +0000

    Rear wheels work

--- app/raspberry_pi/tests/pca9685/test_front_wheels.cpp
+++ app/raspberry_pi/tests/pca9685/test_front_wheels.cpp
@@ -2,12 +2,11 @@
 #include "PCA9685.h"
 #include <iostream>
 #include <stdio.h>
-//#include <ncurses.h>
-#include <thread>         // std::thread
-#include <vector>         // std::vector
+// #include <ncurses.h>
+#include <thread> // std::thread
+#include <vector> // std::vector
 #include <unistd.h>
 #include <string>
-
 
 #include <string.h>
 
@@ -19,72 +18,75 @@ using namespace std;
 
 int offset = 0;
 
-//motor channels
+// motor channels
 int chanh = 0;
 int chanv = 1;
 
-//Declaration of Functions used ==================================
-int setAngle(int& angle, PCA9685 pwm, int& channel);
+// Declaration of Functions used ==================================
+int setAngle(int &angle, PCA9685 pwm, int &channel);
 
-
-//def map(self, x, in_min, in_max, out_min, out_max):
-int map (int x, int in_min, int in_max, int out_min, int out_max) {
-        return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+// def map(self, x, in_min, in_max, out_min, out_max):
+int map(int x, int in_min, int in_max, int out_min, int out_max)
+{
+    return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
 }
 
-//def _angle_to_analog(self, angle):
-int setAngleToAnalog(int angle) {
-      float pulse_wide;
-      int analog_value;
-      
-      pulse_wide = map(angle,0,180,MIN_PULSE_WIDTH,MAX_PULSE_WIDTH);
-      analog_value = int(float(pulse_wide) /  1000000 * FREQUENCY * 4096);
-      return (analog_value);
+// def _angle_to_analog(self, angle):
+int setAngleToAnalog(int angle)
+{
+    float pulse_wide;
+    int analog_value;
+
+    pulse_wide = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+    analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
+    return (analog_value);
 }
 
-int setAngle(int& angle, PCA9685 pwm, int& channel) {
+int setAngle(int &angle, PCA9685 pwm, int &channel)
+{
     int val = 0;
 
-    if (angle > 180) {
-       angle = 179;
+    if (angle > 180)
+    {
+        angle = 179;
     }
-    if (angle < 0) {
-       angle = 1;
+    if (angle < 0)
+    {
+        angle = 1;
     }
-    
+
     val = setAngleToAnalog(angle);
-    //not sure what offset does
+    // not sure what offset does
     val += offset;
 
-    //setPWM(self, channel, on, off
-    //channel: The channel that should be updated with the new values (0..15)
-    //on: The tick (between 0..4095) when the signal should transition from low to high
-    //off:the tick (between 0..4095) when the signal should transition from high to low
-    
-    pwm.setPWM(channel,0,val);
-    //usleep(30);
+    // setPWM(self, channel, on, off
+    // channel: The channel that should be updated with the new values (0..15)
+    // on: The tick (between 0..4095) when the signal should transition from low to high
+    // off:the tick (between 0..4095) when the signal should transition from high to low
+
+    pwm.setPWM(channel, 0, val);
+    // usleep(30);
     cout << "Channel: " << channel << "\tSet to angle: " << angle << "\tVal: " << val << endl;
-    return(0);
+    return (0);
 }
 
+int main()
+{
 
-int main () {
+    int homeh = 90;
+    int homev = 90;
 
-        int homeh = 90;
-	int homev = 90;
-	
-	//make sure you use the right address values.
-	PCA9685 pwm;
-	pwm.init(1,0x40);
-	usleep(1000 * 100);
-	cout << "Setting frequency: " << FREQUENCY << endl;
-	pwm.setPWMFreq (FREQUENCY);
-	usleep(1000 * 1000);
+    // make sure you use the right address values.
+    PCA9685 pwm;
+    pwm.init(1, 0x40);
+    usleep(1000 * 100);
+    cout << "Setting frequency: " << FREQUENCY << endl;
+    pwm.setPWMFreq(FREQUENCY);
+    usleep(1000 * 1000);
 
-        cout << "Returning to HOME position." << endl;
-	setAngle(homeh, pwm, chanh);
-        setAngle(homev, pwm, chanv);
+    cout << "Returning to HOME position." << endl;
+    setAngle(homeh, pwm, chanh);
+    setAngle(homev, pwm, chanv);
 
-
-	return 0;
-} 
+    return 0;
+}
--- app/raspberry_pi/tests/tb6612/test_rear_wheels.cpp
+++ app/raspberry_pi/tests/tb6612/test_rear_wheels.cpp
@@ -1,78 +1,176 @@
+#include <pigpio.h>
 #include <iostream>
+#include <memory>
 #include <thread>
+#include <chrono>
 
 #include "PCA9685.h"
-#include "TB6612.h"
 
-#include "pigpio.h"
-#include "pigpiod_if2.h"
+static constexpr int Motor_A = 17;
+static constexpr int Motor_B = 27;
+static constexpr int PWM_A = 4;
+static constexpr int PWM_B = 5;
 
-void setupGPIOMotor();
+// Made with ChatGPT
 
-class BackWheels
-{
-private:
-    static constexpr int motor_A = 17;
-    static constexpr int motor_B = 27;
-
-    static constexpr int pwm_A = 4;
-    static constexpr int pwm_B = 5;
-
+class TB6612_Motor {
 public:
-    BackWheels(const int &bus_number = 1)
-    {
-        this->pwm.init(bus_number, 0x40);
-        this->right_wheel = std::make_unique<tb6612::Motor>(tb6612::Motor(motor_A, pwm_A, forward_A));
-        this->left_wheel = std::make_unique<tb6612::Motor>(tb6612::Motor(motor_B, pwm_B, forward_B));
+    TB6612_Motor(int motor_pin, int pwm_pin) : motor_pin_(motor_pin), pwm_pin_(pwm_pin), offset_(1) {
+        gpioSetMode(this->motor_pin_, PI_OUTPUT);
+        gpioSetMode(this->pwm_pin_, PI_OUTPUT);
+        gpioWrite(this->motor_pin_, 0);
+        gpioPWM(this->pwm_pin_, 0);
     }
 
-    void forward()
-    {
-        this->right_wheel->forward();
-        this->left_wheel->forward();
+    void setPWM(int value) {
+        gpioPWM(this->pwm_pin_, value);
     }
 
-    void backward()
-    {
-        this->right_wheel->backward();
-        this->left_wheel->backward();
+    void forward() {
+        gpioWrite(this->motor_pin_, this->offset_);
     }
 
-    void stop()
-    {
-        this->right_wheel->stop();
-        this->left_wheel->stop();
+    void backward() {
+        gpioWrite(this->motor_pin_, 1 - this->offset_);
     }
 
-    void setSpeed(int speed)
-    {
-        this->right_wheel->setSpeed(speed);
-        this->left_wheel->setSpeed(speed);
-        this->speed = speed;
+    void stop() {
+        gpioWrite(this->motor_pin_, 0);
+        gpioPWM(this->pwm_pin_, 0);
+    }
+
+    void setOffset(int offset) {
+        this->offset_ = offset;
     }
 
 private:
-    bool forward_A = true;
-    bool forward_B = true;
-
-    std::unique_ptr<tb6612::Motor> right_wheel;
-    std::unique_ptr<tb6612::Motor> left_wheel;
-
-    PCA9685 pwm = PCA9685();
-
-    int speed = 0;
+    int motor_pin_;
+    int pwm_pin_;
+    int offset_;
 };
 
-int main()
-{
-    std::cout << "Initializing\n";
+class Back_Wheels {
+public:
+    Back_Wheels(int bus_number = 1) : left_wheel_(std::make_unique<TB6612_Motor>(Motor_A, PWM_A)),
+                                      right_wheel_(std::make_unique<TB6612_Motor>(Motor_B, PWM_B)) {
+        this->forward_A_ = true;
+        this->forward_B_ = true;
+
+        std::cout << "Initializing PCA9685" << std::endl;
+        this->pca9685_.init(bus_number, 0x40);
+
+        this->speed_ = 0;
+    }
+
+    void forward() {
+        this->left_wheel_->forward();
+        this->right_wheel_->forward();
+        std::cout << "Forward" << std::endl;
+    }
+
+    void backward() {
+        this->left_wheel_->backward();
+        this->right_wheel_->backward();
+        std::cout << "Backward" << std::endl;
+    }
+
+    void stop() {
+        this->left_wheel_->stop();
+        this->right_wheel_->stop();
+        std::cout << "Stop" << std::endl;
+    }
+
+    int getSpeed() const {
+        return this->speed_;
+    }
+
+    void setSpeed(int speed) {
+        this->speed_ = speed;
+        this->pca9685_.setPWM(PWM_A, 0, speed * 40);
+        this->pca9685_.setPWM(PWM_B, 0, speed * 40);
+        std::cout << "Set speed to " << this->speed_ << std::endl;
+    }
+
+    void ready() {
+        this->left_wheel_->setOffset(this->forward_A_);
+        this->right_wheel_->setOffset(this->forward_B_);
+        this->stop();
+        std::cout << "Ready" << std::endl;
+    }
+
+    void calibration() {
+        this->setSpeed(50);
+        this->forward();
+        this->cali_forward_A_ = this->forward_A_;
+        this->cali_forward_B_ = this->forward_B_;
+        std::cout << "Calibration" << std::endl;
+    }
+
+    void caliLeft() {
+        this->cali_forward_A_ = (1 + this->cali_forward_A_) & 1;
+        this->left_wheel_->setOffset(this->cali_forward_A_);
+        this->forward();
+        std::cout << "CaliLeft" << std::endl;
+    }
+
+    void caliRight() {
+        this->cali_forward_B_ = (1 + this->cali_forward_B_) & 1;
+        this->right_wheel_->setOffset(this->cali_forward_B_);
+        this->forward();
+        std::cout << "CaliRight" << std::endl;
+    }
+
+    void caliOK() {
+        this->forward_A_ = this->cali_forward_A_;
+        this->forward_B_ = this->cali_forward_B_;
+        this->stop();
+        std::cout << "CaliOK" << std::endl;
+    }
+
+private:
+    std::unique_ptr<TB6612_Motor> left_wheel_;
+    std::unique_ptr<TB6612_Motor> right_wheel_;
+    PCA9685 pca9685_;
+    int forward_A_;
+    int forward_B_;
+    int cali_forward_A_;
+    int cali_forward_B_;
+    int speed_;
+};
+
+void test() {
+    std::cout << "Initializing GPIO" << std::endl;
     gpioInitialise();
-    std::cout << "Completed Initializing\n";
-    BackWheels wheels;
-    std::cout << "Started wheels\n";
-    wheels.setSpeed(10);
-    std::cout << "Set speed\n";
-    wheels.forward();
-    std::cout << "Moving forward\n";
+    Back_Wheels back_wheels;
+    const float DELAY = 0.01f;
+    try {
+        back_wheels.forward();
+        for (int i = 0; i <= 100; ++i) {
+            back_wheels.setSpeed(i);
+            std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(DELAY * 1000)));
+        }
+        for (int i = 100; i >= 0; --i) {
+            back_wheels.setSpeed(i);
+            std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(DELAY * 1000)));
+        }
+
+        back_wheels.backward();
+        for (int i = 0; i <= 100; ++i) {
+            back_wheels.setSpeed(i);
+            std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(DELAY * 1000)));
+        }
+        for (int i = 100; i >= 0; --i) {
+            back_wheels.setSpeed(i);
+            std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(DELAY * 1000)));
+        }
+    } catch (...) {
+        back_wheels.stop();
+    }
+    std::cout << "Terminating GPIO" << std::endl;
+    gpioTerminate();
+}
+
+int main() {
+    test();
     return 0;
 }
