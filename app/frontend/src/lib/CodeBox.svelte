43345a776e589b3defe21e1288fc098537d8855f
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-01-17 10:59:22 +0000

    Add CodeBox

---
+++ app/frontend/src/lib/CodeBox.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	// If this does not work then try preview
+	import CodeMirror from "svelte-codemirror-editor";
+	import { xml } from "@codemirror/lang-xml";
+	let value = "";
+
+	// From @codemirror/lang-xml repository
+	let schema1 = {
+		elements: [
+			{
+				name: "doc",
+				top: true,
+				attributes: [
+					"attr1",
+					"attr2",
+					{
+						name: "attr3",
+						values: ["x", "y"],
+						completion: { type: "keyword" },
+					},
+				],
+				children: ["head", "body"],
+				completion: { type: "keyword" },
+			},
+			{ name: "head", attributes: ["attr2"] },
+			{ name: "body", children: [] },
+		],
+		attributes: [
+			{ name: "attr2", values: ["one", "two"] },
+			{ name: "attrglobal", global: true },
+		],
+	};
+</script>
+
+<div class="text-left">
+	<CodeMirror class="w-60 h-60" bind:value lang={xml(schema1)} />
+</div>
--- app/frontend/src/routes/Room.svelte
+++ app/frontend/src/routes/Room.svelte
@@ -2,11 +2,11 @@
 	import ChatBox from "../lib/ChatBox.svelte";
 	import LidarStream from "../lib/LidarStream.svelte";
 	import UrlPattern from "url-pattern";
+    import CodeBox from "../lib/CodeBox.svelte";
 
 	const room_pattern = new UrlPattern("#/room/:room");
 	let room_name = room_pattern.match(window.location.hash).room;
 
-	let text = "";
 	const websocket: WebSocket = new WebSocket(
 		`ws://${location.host}/ws/room?request=create&room_name=${room_name}`,
 	);
@@ -14,3 +14,4 @@
 
 <LidarStream {room_name} {websocket} />
 <ChatBox {websocket} />
+<CodeBox />
