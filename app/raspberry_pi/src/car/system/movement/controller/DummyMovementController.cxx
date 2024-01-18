eb6d595f6ae38a98da7f65b210e39de8c52625f6
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-01-18 14:54:35 +0000

    Add logs for dummy

--- app/raspberry_pi/src/car/system/movement/controller/DummyMovementController.cxx
+++ app/raspberry_pi/src/car/system/movement/controller/DummyMovementController.cxx
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <spdlog/spdlog.h>
+
 #include "AbstractMovementController.cxx"
 
 namespace car::system::movement::controller
@@ -13,39 +15,42 @@ namespace car::system::movement::controller
 		void initialize() {};
 
 		void setRearWheelsSpeed(const int& speed) override {
-
+			spdlog::info("[DUMMY] Setting Left Rear Wheel speed to {}", speed);
 		}
 
-		void setRearLeftWheelSpeed(const int& speed) override {
-
+		void setRearLeftWheelSpeed(const int& speed) override
+		{
+			spdlog::info("[DUMMY] Setting Left Rear Wheel speed to {}", speed);
 		}
 
-		void setRearRightWheelSpeed(const int& speed) override {
-
+		void setRearRightWheelSpeed(const int& speed) override
+		{
+			spdlog::info("[DUMMY] Setting Right Rear Wheel speed to {}", speed);
 		}
 
 		void setFrontWheelsAngle(const float& angle) override
 		{
-		
+			spdlog::info("[DUMMY] Setting Front Wheels angle to {}", angle);
 		}
 
 		void setCameraServo1Angle(const float& angle) override
 		{
-		
+			spdlog::info("[DUMMY] Setting Camera Servo 1 angle to {}", angle);
 		}
 
 		void setCameraServo2Angle(const float& angle) override
 		{
-
+			spdlog::info("[DUMMY] Setting Camera Servo 2 angle to {}", angle);
 		}
 
 		void setRearWheelDirectionToForward() override {
-
+			spdlog::info("[DUMMY] Both Rear Wheels are set to to move forward");
 		}
 
 		void setRearWheelDirectionToBackward() override {
-
+			spdlog::info("[DUMMY] Both Rear Wheels are set to move backward");
 		}
+
 	private:
 	};
 } // namespace car::system::movement::controller
