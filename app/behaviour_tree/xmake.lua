9e59063d134e39e23897adde9015302fa1bfeea2
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-01-18 17:41:17 +0000

    Add pugixml to behaviour_tree

--- app/behaviour_tree/xmake.lua
+++ app/behaviour_tree/xmake.lua
@@ -2,12 +2,14 @@ add_rules("mode.debug", "mode.release")
 
 add_requires("fmt")
 add_requires("tl_expected")
+add_requires("pugixml")
 
 target("behaviour_tree")
     set_kind("$(kind)")
     set_languages("cxx17")
     add_packages("fmt")
     add_packages("tl_expected")
+    add_packages("pugixml")
 
     add_includedirs("include", {public = true})
     add_headerfiles("include/(**.hpp)")
