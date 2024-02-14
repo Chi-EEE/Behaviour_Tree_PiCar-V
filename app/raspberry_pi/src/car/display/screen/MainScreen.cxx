57dac807f9df09e926b2fc7f0f9efa61ad97a5fa
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-02-14 10:51:25 +0000

    Remove BehaviourTreePanel functionallity

--- app/raspberry_pi/src/car/display/screen/MainScreen.cxx
+++ app/raspberry_pi/src/car/display/screen/MainScreen.cxx
@@ -11,7 +11,6 @@
 
 #include "../component/main/MainButton.cxx"
 #include "../component/main/MainExitModal.cxx"
-#include "../component/main/BehaviourTreePanel.cxx"
 
 using namespace car::system;
 using namespace car::display::component::main;
@@ -23,7 +22,7 @@ namespace car::display::screen
 	class MainScreen
 	{
 	public:
-		MainScreen(std::shared_ptr<CarSystem> car_system, std::function<void()> exit) : car_system(car_system), main_button(car_system, box), main_exit_modal(exit), behaviour_tree_panel(car_system)
+		MainScreen(std::shared_ptr<CarSystem> car_system, std::function<void()> exit) : car_system(car_system), main_button(car_system, box), main_exit_modal(exit)
 		{
 			auto main_button_component = this->main_button.element();
 
@@ -32,12 +31,11 @@ namespace car::display::screen
 			auto show_exit_modal = [&]
 				{ this->main_exit_modal.exit_modal_shown = true; };
 
-			this->left_side =
+			this->main_screen =
 				Container::Vertical({
 					Renderer([&]()
 							 {
-							// TODO: Fix the y size of the canvas
-							auto c = Canvas(box.x_max, box.y_max * 3);
+							auto c = Canvas(this->box.x_max, this->box.y_max * 3);
 							std::vector<rplidar::Measure> scan_data = this->car_system->getScanData();
 							for (auto& point : scan_data) {
 								const double angle = point.angle;
@@ -47,28 +45,19 @@ namespace car::display::screen
 								const int y = distance * std::sin(angleInRadians);
 								c.DrawBlockLine(50, 50, x, y, Color::Blue);
 							}
-							return canvas(std::move(c)); }),
+							return canvas(std::move(c)); }) | center,
 						Renderer([&]
 								 { return separator(); }),
 						Container::Horizontal({
-							main_button_component | center,
+							main_button_component | flex,
 							Renderer([&]
 									 { return separator(); }),
-							Button("Quit", std::move(show_exit_modal), ButtonOption::Animated()) | center,
-						}),
+							Button("Quit", std::move(show_exit_modal), ButtonOption::Animated()) | flex,
+						}) | center,
 					}) |
 				xflex;
 
-			auto behaviour_tree_panel_component = this->behaviour_tree_panel.element();
-
-			this->main_component = Container::Horizontal(
-				{
-					left_side,
-					Renderer([&]
-							 { return separator(); }),
-					behaviour_tree_panel_component,
-				}) |
-				border | flex | reflect(box);
+			this->main_component = this->main_screen | border | flex | reflect(this->box);
 
 			this->main_component |= Modal(main_exit_modal_component, &main_exit_modal.exit_modal_shown);
 		}
@@ -86,13 +75,9 @@ namespace car::display::screen
 		MainButton main_button;
 		MainExitModal main_exit_modal;
 
-		BehaviourTreePanel behaviour_tree_panel;
-
-		Component b;
 		Component info;
 
-		Component left_side;
-		Component right_side;
+		Component main_screen;
 
 		Component main_component;
 	};
