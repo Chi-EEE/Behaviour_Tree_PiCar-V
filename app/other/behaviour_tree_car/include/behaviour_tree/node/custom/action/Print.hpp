e8a8996a8e6af6f1a6d0c1d228c95e5f67b44ea7
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-03-20 08:07:38 +0000

    Use spdlog if running defined

--- app/other/behaviour_tree_car/include/behaviour_tree/node/custom/action/Print.hpp
+++ app/other/behaviour_tree_car/include/behaviour_tree/node/custom/action/Print.hpp
@@ -10,43 +10,46 @@
 #include "behaviour_tree/Context.h"
 #ifndef BEHAVIOUR_TREE_DISABLE_RUN
 #include "behaviour_tree/CarContext.hpp"
+#include <spdlog/spdlog.h>
 #endif // !BEHAVIOUR_TREE_DISABLE_RUN
 
 namespace behaviour_tree::node::custom::action
 {
-	class Print final : public CustomNode
-	{
-	public:
-		Print(const std::string& name, const std::string& text) : CustomNode(name), text(text)
-		{
-		}
+    class Print final : public CustomNode
+    {
+    public:
+        Print(const std::string &name, const std::string &text) : CustomNode(name), text(text)
+        {
+        }
 
-		const Status run(const int& tick_count, std::shared_ptr<Context> context) final override
-		{
-// #ifndef BEHAVIOUR_TREE_DISABLE_RUN
-// 			std::shared_ptr<CarContext> car_context = std::dynamic_pointer_cast<CarContext>(context);
-// #else
-// 			std::cout << this->getText();
-// #endif
-			spdlog::info(this->getText());
-			return Status::Success;
-		}
+        const Status run(const int &tick_count, std::shared_ptr<Context> context) final override
+        {
+#ifndef BEHAVIOUR_TREE_DISABLE_RUN
+            std::shared_ptr<CarContext> car_context = std::dynamic_pointer_cast<CarContext>(context);
+            spdlog::info(this->getText());
+#else
+            std::cout << this->getText();
+#endif
+            return Status::Success;
+        }
 
-		const std::string& getText() const {
-			return this->text;
-		}
+        const std::string &getText() const
+        {
+            return this->text;
+        }
 
-		const std::string toString() const final override {
-			const std::string& name = this->getName();
-			if (name != "")
-				return fmt::format(R"(<Action:Print name='{}' text='{}'/>)", name, this->getText());
-			else
-				return fmt::format(R"(<Action:Print text='{}'/>)", this->getText());
-		}
+        const std::string toString() const final override
+        {
+            const std::string &name = this->getName();
+            if (name != "")
+                return fmt::format(R"(<Action:Print name='{}' text='{}'/>)", name, this->getText());
+            else
+                return fmt::format(R"(<Action:Print text='{}'/>)", this->getText());
+        }
 
-	private:
-		const std::string text;
-	};
+    private:
+        const std::string text;
+    };
 }
 
 #endif
