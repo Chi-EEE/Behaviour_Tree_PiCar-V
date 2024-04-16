3f19941f7f771cc0cd650d548c479c2df3c22d0f
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-04-16 11:05:18 +0100

    Remove unused policies

--- app/rpi/xmake.lua
+++ app/rpi/xmake.lua
@@ -1,8 +1,3 @@
-set_policy("build.sanitizer.thread", true)
-set_policy("build.sanitizer.memory", true)
-set_policy("build.sanitizer.leak", true)
-set_policy("build.sanitizer.undefined", true)
-
 add_repositories("local-repo repository")
 
 includes("common/xmake.lua", "tui/xmake.lua", "behaviour_tree/xmake.lua")
