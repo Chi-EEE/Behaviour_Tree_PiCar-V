5c1e44c97ac9c64371612cd018f222910a8f8aa3
Chi-EEE <Chi-EEE@users.noreply.github.com>
2024-04-19 19:07:50 +0000

    deploy: 2fb4900f76437c20cdbd63c1f73d3bc76a5e828b

--- admin_panel/dir_f832923ad3cb060bc87ad85e68b8a1c3.js
+++
@@ -1,4 +0,0 @@
-var dir_f832923ad3cb060bc87ad85e68b8a1c3 =
-[
-    [ "index.html", "index_8html.html", null ]
-];
--- admin_panel/search/files_5.js
+++ admin_panel/search/files_5.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['index_2ehtml_0',['index.html',['../index_8html.html',1,'']]],
-  ['iplist_2esvelte_1',['IPList.svelte',['../IPList_8svelte.html',1,'']]]
+  ['iplist_2esvelte_0',['IPList.svelte',['../IPList_8svelte.html',1,'']]]
 ];
